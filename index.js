a3353ce6a77b86331034359a51f74761f7f49e45
Paul von Hagen <pvonhagen26@gmail.com>
2020-09-09 04:32:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function User(name, email) {
+    this.name = name;
+    this.email = email;
+    this.sayHello = function() {
+        console.log(`Hello everybody, my name is ${this.name} whom you've been
+mailing at ${this.email}!`);
+    };
+}
+Scooter with year, color, and model properties
+Driver with name, age, and experience properties
+PickupLocation with address and city properties
+
+function Scooter(year, color, model){
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+function Driver(name, age, experience){
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
