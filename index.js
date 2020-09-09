503ca58df1d1a841ca9c535c7d0c61d7633c99a5
Paul von Hagen <pvonhagen26@gmail.com>
2020-09-09 04:33:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function Driver(name, age, experience){
   this.experience = experience;
 }
 
-funciton PickupLocation(address, city){
+function PickupLocation(address, city){
   this.address = address;
   this.city = city;
 }
